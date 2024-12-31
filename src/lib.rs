1760a8ea0fc707609bc4b421b5fe6be80a8e1920
Fifixex <122852085+Fifixex@users.noreply.github.com>
2024-12-31 13:27:18 +0000

    ♻️  refactor: add `ThumbnailGenerator` struct and implements simple methods

--- src/lib.rs
+++ src/lib.rs
@@ -74,18 +74,6 @@
 //!
 //! Arem is dual-licensed under Apache 2.0 and MIT. You can choose the license
 //! that best suits your needs.
+mod thumbnail;
 
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+pub use thumbnail::ThumbnailGenerator;
---
+++ src/thumbnail.rs
@@ -0,0 +1,37 @@
+#[derive(Debug, Clone)]
+pub struct ThumbnailGenerator {
+    quality: u8,
+    resolution: (u32, u32),
+}
+
+impl Default for ThumbnailGenerator {
+    fn default() -> Self {
+        Self {
+            quality: 80,
+            resolution: (320, 240),
+        }
+    }
+}
+
+impl ThumbnailGenerator {
+    /// Creates a new `ThumbnailGenerator` with default options.
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    pub fn quality(mut self, quality: u8) -> Self {
+        if (0..=100).contains(&quality) {
+            panic!("Quality must be between 0 and 100");
+        }
+        self.quality = quality;
+        self
+    }
+
+    pub fn resolution(mut self, width: u32, height: u32) -> Self {
+        if width == 0 || height == 0 {
+            panic!("Resolution must be greater than 0");
+        }
+        self.resolution = (width, height);
+        self
+    }
+}
